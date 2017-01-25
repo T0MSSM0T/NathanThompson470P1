d16d34809fbe24ac1d35e01473a322cfa23b09a5
Nathan Johnson <johns2nt@Nathans-MacBook-Pro.local>
2017-01-24 23:29:08 -0500

    linked list implementation added

---
+++ linked_list.h
@@ -0,0 +1,28 @@
+#ifndef __LINKED_LIST__
+#define __LINKED_LIST__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/****
+* Linked list implementation in the form of a queue.
+*
+* Author: Nathan Johnson
+*/
+
+typedef struct __attribute__((__packed__)) node {
+	struct node * next;
+	char * action;
+}node_t;
+
+node_t * first;
+node_t * last;
+
+void add_action(char* act);
+char * pull_action();
+bool is_empty();
+void print_list();
+void destroy();
+
+#endif
